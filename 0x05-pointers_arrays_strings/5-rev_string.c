5151e21d5d2eccc92a95ed495b4b942055cf59c6
deenjeri <sherrydiana15@gmail.com>
2023-02-27 11:36:22 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,24 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - displays my frustration level
- *
- * Return: void
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
  */
 
-int main(void)
+void rev_string(char *s)
 {
-printf("ALX is annoying\n");
-printf("I have tried fixing this for the 25th time\n");
-printf("I am tired of the non beginner friendly program\n");
-printf("Mtchew\n");
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-return (0);
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
 }
